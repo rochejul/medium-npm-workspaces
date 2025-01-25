a71c2ae61984c173bf551756fe75e8e129cc932f
Julien Roche <roche.jul@gmail.com>
2025-01-25 21:00:34 +0100

    feat(workspace): first step done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.info(`I'm the root package`);
+import { sum } from "@example/module-a";
+import { sub } from "@example/module-b";
+
+const result = sub(sum(25, 42), 7);
+console.info("Result:", result);
